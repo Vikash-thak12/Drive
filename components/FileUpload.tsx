fbba46b3d50506d960a1d39c335dd000cbc2db69
Vikash Thakur <thakvikash622@gmail.com>
2024-12-22 17:57:25 +0530

    Thumbnail part is completed

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -3,20 +3,23 @@ import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from './ui/button'
 import Image from 'next/image'
+import { convertFileToUrl, getFileType } from '@/lib/utils'
+import Thumbnail from './Thumbnail'
 
 interface FileProps {
-  ownerId: string, 
-  accountId: string, 
-  path: string, 
+  ownerId: string,
+  accountId: string,
+  path: string,
   className?: string,
 }
 
-const FileUpload = ({ ownerId, accountId, path, className}: FileProps) => {
+const FileUpload = ({ ownerId, accountId, path, className }: FileProps) => {
   const [files, setFiles] = useState<File[]>([])
 
 
-  const onDrop = useCallback((acceptedFiles) => {
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
     // Do something with the files
+    setFiles(acceptedFiles)
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
@@ -27,6 +30,29 @@ const FileUpload = ({ ownerId, accountId, path, className}: FileProps) => {
         <Image src={"/assets/icons/upload.svg"} alt='Upload' width={24} height={24} />
         <p>Upload</p>
       </Button>
+      {
+        files.length > 0 && <ul className='uploader-preview-list'>
+          <h4 className='h4 text-light-100'>Uploading</h4>
+          {
+            files.map((file, index) => {
+              const { type, extension } = getFileType(file.name)
+              // console.log("the file name is", file.name)
+              // the file name is Screenshot (29).png
+              return (
+                <li key={`${file.name}-${index}`} className='uploader-preview-item'>
+                  <div>
+                    <Thumbnail
+                      type={type}
+                      extension={extension}
+                      url={convertFileToUrl(file)}
+                    />
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      }
     </div>
   )
 }
