17d19ca55128efa5e3858b3a0bce4ab8175fd606
Vikash Thakur <thakvikash622@gmail.com>
2024-12-22 18:26:03 +0530

    Fixed: Uploading functionality

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
-import { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from './ui/button'
 import Image from 'next/image'
@@ -19,9 +20,15 @@ const FileUpload = ({ ownerId, accountId, path, className }: FileProps) => {
 
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
     // Do something with the files
-    setFiles(acceptedFiles)
+    setFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const handleRemoveFile = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, fileName: string) => {
+    // Prevents the click event on the "Remove" icon from propagating to its parent elements.
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName))
+  }
 
   return (
     <div {...getRootProps()} className='cursor-pointer'>
@@ -31,27 +38,38 @@ const FileUpload = ({ ownerId, accountId, path, className }: FileProps) => {
         <p>Upload</p>
       </Button>
       {
-        files.length > 0 && <ul className='uploader-preview-list'>
-          <h4 className='h4 text-light-100'>Uploading</h4>
-          {
-            files.map((file, index) => {
-              const { type, extension } = getFileType(file.name)
-              // console.log("the file name is", file.name)
-              // the file name is Screenshot (29).png
-              return (
-                <li key={`${file.name}-${index}`} className='uploader-preview-item'>
-                  <div>
-                    <Thumbnail
-                      type={type}
-                      extension={extension}
-                      url={convertFileToUrl(file)}
-                    />
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
+        files.length > 0 && (
+          <ul className='uploader-preview-list'>
+            <h4 className='h4 text-light-100'>Uploading</h4>
+            {
+              files.map((file, index) => {
+                const { type, extension } = getFileType(file.name)
+                // console.log("the file name is", file.name)
+                // the file name is Screenshot (29).png
+                return (
+                  <li key={`${file.name}-${index}`} className='uploader-preview-item'>
+                    <div className='flex items-center gap-5'>
+
+                      <Thumbnail
+                        type={type}
+                        extension={extension}
+                        url={convertFileToUrl(file)}
+                      />
+
+                      <div className='preview-item-name'>
+                        {file.name}
+                        <Image src={"/assets/icons/file-loader.gif"} alt='Uploading' width={100} height={100} />
+                      </div>
+
+                    </div>
+
+                    <Image src={"/assets/icons/remove.svg"} alt='Remove' width={32} height={32} onClick={(e) => handleRemoveFile(e, file.name)} />
+                  </li>
+                )
+              })
+            }
+          </ul>
+        )
       }
     </div>
   )
