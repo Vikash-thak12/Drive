74fd91e718c6ad16ebaa3f7e4898ac136112fdcf
Vikash Thakur <thakvikash622@gmail.com>
2024-12-22 17:33:08 +0530

    Working on file uploading functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,17 +4,17 @@ import Sidebar from '@/components/Sidebar'
 import { getCurrentUser } from '@/lib/actions/user.action'
 import React from 'react'
 
-const layout = async ({ children}: { children: React.ReactNode}) => {
-    const currentUser = await getCurrentUser()
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser()
 
   return (
     <main className='flex h-screen'>
-        <Sidebar {...currentUser} />
+      <Sidebar {...currentUser} />
       <section className='flex flex-1 flex-col h-full'>
         <MobileNav {...currentUser} />
         <Header />
         <div className='h-full bg-light-400 flex-1 px-5 py-7 overflow-auto remove-scrollbar lg:mb-7 rounded-[30px] lg:mr-7'>
-            {children}
+          {children}
         </div>
       </section>
     </main>
--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+import { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Button } from './ui/button'
+import Image from 'next/image'
 
-const FileUpload = () => {
+interface FileProps {
+  ownerId: string, 
+  accountId: string, 
+  path: string, 
+  className?: string,
+}
+
+const FileUpload = ({ ownerId, accountId, path, className}: FileProps) => {
+  const [files, setFiles] = useState<File[]>([])
+
+
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   return (
-    <div>
-      FileUpload
+    <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+      <Button className='uploader-button' type='button'>
+        <Image src={"/assets/icons/upload.svg"} alt='Upload' width={24} height={24} />
+        <p>Upload</p>
+      </Button>
     </div>
   )
 }
