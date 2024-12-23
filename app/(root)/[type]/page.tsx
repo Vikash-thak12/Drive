06d93d69fc771fb3a7eb20d0977891d896047faf
Vikash Thakur <thakvikash622@gmail.com>
2024-12-23 18:29:45 +0530

    cls

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,18 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import Sort from '@/components/Sort';
 import React from 'react'
 
 interface SearchParamProps {
   params: { type: string }; // Accessing type directly from params
-  searchParams?: { [key: string]: string | string[] | undefined };
+  // searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-const DynamicPage = ({ params, searchParams }: SearchParamProps) => {
+const DynamicPage = ({ params }: SearchParamProps) => {
   const { type } = params;
 
   return (
-    <section>
-      <h1>{type}</h1>
-    </section>
+    <div className='sort-container'>
+      <section className='w-full'>
+        <h1 className='font-bold text-3xl capitalize'>{type}</h1>
+        <div className='total-size-section'>
+          <p>Total: <span className='font-bold'>12 KB</span></p>
+          <div className='sort-container'>
+              <p>Sort By: </p>
+              <Sort />
+          </div>
+        </div>
+      </section>
+
+      {/* Render the filex`s  */}
+    </div>
   )
 }
 
