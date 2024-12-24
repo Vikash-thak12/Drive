38cf460f2505b906267554a94659ae5d22e378ea
Vikash Thakur <thakvikash622@gmail.com>
2024-12-24 23:38:30 +0530

    Fixed: Sorting

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+"use client"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation"
+
 
 const Sort = () => {
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+
   return (
-    <div>
-      Sort
-    </div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent>
+        {
+          sortTypes.map((type) => (
+            <SelectItem key={type.label} value={type.value}>{type.label}</SelectItem>
+          ))
+        }
+      </SelectContent>
+    </Select>
+
   )
 }
 
