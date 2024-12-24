f4fe475d529bc76efb13419cdeb8b4e67027ab96
Vikash Thakur <thakvikash622@gmail.com>
2024-12-24 20:55:50 +0530

    Implemented: queries for sort and search

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -2,18 +2,20 @@
 import Card from '@/components/Card';
 import Sort from '@/components/Sort';
 import { getFiles } from '@/lib/actions/file.actions';
+import { getFileTypesParams } from '@/lib/utils';
 import { Models } from 'node-appwrite';
 import React from 'react'
 
-interface SearchParamProps {
-  params: { type: string }; // Accessing type directly from params
-  // searchParams?: { [key: string]: string | string[] | undefined };
-}
+const DynamicPage = async ({ params, searchParams }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || ""; 
+  const sort = ((await searchParams)?.sort as string) || "";
 
-const DynamicPage = async ({ params }: SearchParamProps) => {
-  const { type } = await params;
 
-  const files = await getFiles();
+  // this is for different types like document, media, others, images
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
   // console.log("the files are", files)
 
   return (
